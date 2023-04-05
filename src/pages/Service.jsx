c5fa45a274ff1a3b5f38b36921de56d12ae0b439
Haadiiii <hammali99@gmail.com>
2023-04-05 08:00:10 +0500

    Update service page

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -5,13 +5,9 @@ import BrowserJobs from "../components/serviceDetails/BrowserJobs";
 
 const Service = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const jobs = [
-    <UpcomingJobs />,
-  ];
+  const jobs = [<UpcomingJobs />];
 
-  const browse = [
-    <BrowserJobs />,
-  ];
+  const browse = [<BrowserJobs />];
 
   const handleSwipe = (index, indexLatest) => {
     if (index < indexLatest) {
@@ -22,14 +18,36 @@ const Service = () => {
   };
 
   return (
-    <div className="flex flex-col bg-black h-screen w-screen">
-      <div className="flex bg-black gap-4 p-4">
+    <div className="flex flex-col bg-slate-900 h-screen w-screen">
+      {/* back arrow to home */}
+      <div
+        className="flex flex-row py-2 px-4"
+        onClick={() => {
+          window.location.href = "/";
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-8 w-6 text-white"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M10 19l-7-7m0 0l7-7m-7 7h18"
+          />
+        </svg>
+      </div>
+      <div className="flex gap-4 px-4">
         <div>
           {" "}
           <img className="w-16 h-16 rounded-full mt-10" src={profile} alt="" />
         </div>
         <div className="flex flex-col mt-12">
-          <h4 className="text-white font-thin">Welcome back</h4>
+          <h4 className="text-white text-sm font-thin">Welcome back</h4>
           <h1 className="text-white font-bold">Anjelina Marcus</h1>
         </div>
       </div>
@@ -37,7 +55,6 @@ const Service = () => {
         <h1 className="text-2xl font-bold mb-5 text-start text-stone-50 ">
           Upcoming Jobs
         </h1>
-
         <div
           className=" flex gap-4 rounded-2xl"
           index={currentIndex}
@@ -50,9 +67,8 @@ const Service = () => {
           ))}
         </div>
       </div>
-
       <div className="flex flex-col p-6">
-        <h1 className="text-2xl font-bold mt-5 text-start text-stone-50 mb-5">
+        <h1 className="text-2xl font-bold mt-1 text-start text-stone-50 mb-5">
           Browse More
         </h1>
 
