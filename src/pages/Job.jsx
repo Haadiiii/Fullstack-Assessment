f69a825542ad7226de44c9176ce1f258534ac59a
Haadiiii <hammali99@gmail.com>
2023-04-05 07:59:56 +0500

    create and update job page

--- src/pages/Job.jsx
+++ src/pages/Job.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import profile from "../assets/Ellipse.png";
+import PostJob from "../components/jobDetails/PostJob";
+import HiredPeople from "../components/jobDetails/HiredPeople";
 
 const Job = () => {
   return (
-    <div>Job</div>
-  )
-}
+    <div className="flex flex-col bg-slate-900 h-screen w-screen">
+      {/* back arrow to home */}
+      <div
+        className="flex flex-row py-2 px-4"
+        onClick={() => {
+          window.location.href = "/";
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-8 w-6 text-white"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M10 19l-7-7m0 0l7-7m-7 7h18"
+          />
+        </svg>
+      </div>
+      <div className="flex gap-4 px-4">
+        <div>
+          {" "}
+          <img className="w-16 h-16 rounded-full mt-10" src={profile} alt="" />
+        </div>
+        <div className="flex flex-col mt-12">
+          <h4 className="text-white text-sm font-thin">Welcome back</h4>
+          <h1 className="text-white font-bold">Anjelina Marcus</h1>
+        </div>
+      </div>
 
-export default Job
+      <div className="flex gap-4 rounded-2xl p-4">
+        <PostJob />
+      </div>
+      <div className="flex flex-col p-6">
+        <h1 className="text-2xl font-bold mt-1 text-start text-stone-50 mb-5">
+          People You've Hired
+        </h1>
+        <div
+          className=" flex gap-4 rounded-2xl"
+        >
+         <HiredPeople /> 
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Job;
